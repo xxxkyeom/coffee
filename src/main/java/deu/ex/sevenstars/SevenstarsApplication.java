76d2cfedcbae527f32cf1ec9f46687eeeea20c77
usingjun <97494494+usingjun@users.noreply.github.com>
2024-09-06 16:37:18 +0900

    First commit

---
+++ src/main/java/deu/ex/sevenstars/SevenstarsApplication.java
@@ -0,0 +1,13 @@
+package deu.ex.sevenstars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevenstarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SevenstarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/deu/ex/sevenstars/SevenstarsApplicationTests.java
@@ -0,0 +1,13 @@
+package deu.ex.sevenstars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevenstarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
