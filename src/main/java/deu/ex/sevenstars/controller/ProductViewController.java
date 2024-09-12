2e6c6beb4eec2b78987d3ec350aea11261d74fd0
wndgh <wndgh6@naver.com>
2024-09-12 12:17:52 +0900

    coffee_09_12_test

--- src/main/java/deu/ex/sevenstars/controller/ProductViewController.java
+++ src/main/java/deu/ex/sevenstars/controller/ProductViewController.java
@@ -1,6 +1,13 @@
 package deu.ex.sevenstars.controller;
 
+import deu.ex.sevenstars.dto.PageRequestDTO;
+import deu.ex.sevenstars.dto.ProductDTO;
+import deu.ex.sevenstars.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +17,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 @Controller
+@RequiredArgsConstructor
 @Log4j2
 public class ProductViewController {
+    private final ProductService productService;
 
-    @GetMapping("/products")
-    public String listProducts() {
+    @GetMapping("/products") //페이징 처리
+    public String pageProduct(@PageableDefault(page = 1)PageRequestDTO pageRequestDTO, Model model) {
+        Page<ProductDTO> productDTOPage = productService.page(pageRequestDTO);
+
         return "product-list"; // 템플릿 이름 (templates/product-list.html)
     }
 
@@ -35,12 +46,6 @@ public class ProductViewController {
         log.info("Price: {}", price);
         log.info("Description: {}", description);
         log.info("image: {}", image);
-
-        model.addAttribute("productName", productName);
-        model.addAttribute("category", category);
-        model.addAttribute("price", price);
-        model.addAttribute("description", description);
-        model.addAttribute("image", image.getOriginalFilename());
 
 
         return "product-list"; //
