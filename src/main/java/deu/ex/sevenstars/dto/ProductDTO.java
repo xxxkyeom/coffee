965c4f7ac35a03a2bbd914c9e2aa9506de5a0195
seongkyeom <xxxkyeom@github.com>
2024-09-12 14:34:33 +0900

    update

--- src/main/java/deu/ex/sevenstars/dto/ProductDTO.java
+++ src/main/java/deu/ex/sevenstars/dto/ProductDTO.java
@@ -6,7 +6,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
